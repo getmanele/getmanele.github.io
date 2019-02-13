8bbb2bae467844f939cffffec4c07bc980de6ea6
getmanele <getmanele@gmail.com>
2019-02-13 14:59:22 +0300

    S&Mitler

--- js/popup.js
+++ js/popup.js
@@ -1,13 +1,13 @@
 // Get the modal
-var modal = document.getElementById('myModal');
+var modal = document.getElementById('myModal', 'myModal1', 'myModal2', 'myModal3');
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
 var img  = document.getElementById('myImg');
 var img2 = document.getElementById('myImg2');
 var img3 = document.getElementById('myImg3');
-// var img4 = document.getElementById('myImg4');
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
+var img4 = document.getElementById('myImg4');
+var modalImg = document.getElementById("img01", "img02", "img03", "img04");
+var captionText = document.getElementById("caption", "caption02", "caption03", "caption04");
 img.onclick = function(){
     modal.style.display = "block";
     modalImg.src = this.src;
@@ -23,11 +23,11 @@ img3.onclick = function(){
     modalImg.src = this.src;
     captionText.innerHTML = this.alt;
 }
-// img4.onclick = function(){
-//     modal.style.display = "block";
-//     modalImg.src = this.src;
-//     captionText.innerHTML = this.alt;
-// }
+img4.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
