d224ad730a4502dc342670c1298f22ec4f91c276
getmanele <getmanele@gmail.com>
2019-03-15 00:56:20 +0300

    add 5th job& dell NY-het

--- js/popup.js
+++ js/popup.js
@@ -1,12 +1,13 @@
 // Get the modal
-var modal = document.getElementById('myModal', 'myModal1', 'myModal2', 'myModal3');
+var modal = document.getElementById('myModal', 'myModal1', 'myModal2', 'myModal3', 'myModal4');
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
 var img  = document.getElementById('myImg');
 var img2 = document.getElementById('myImg2');
 var img3 = document.getElementById('myImg3');
 var img4 = document.getElementById('myImg4');
-var modalImg = document.getElementById("img01", "img02", "img03", "img04");
+var img5 = document.getElementById('myImg5');
+var modalImg = document.getElementById("img01", "img02", "img03", "img04", "img05");
 var captionText = document.getElementById("caption", "caption02", "caption03", "caption04");
 img.onclick = function(){
     modal.style.display = "block";
@@ -24,6 +25,11 @@ img3.onclick = function(){
     captionText.innerHTML = this.alt;
 }
 img4.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+img5.onclick = function(){
     modal.style.display = "block";
     modalImg.src = this.src;
     captionText.innerHTML = this.alt;
