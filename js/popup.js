6575f38b71dc02cd30566b48a3ee7f5008fce00a
getmanele <getmanele@gmail.com>
2019-04-11 17:12:22 +0300

    adaptive-img-myPhoto-v1.7

--- js/popup.js
+++ js/popup.js
@@ -3,6 +3,10 @@ var modal = document.getElementById('myModal', 'myModal1', 'myModal2', 'myModal3
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
 var imgJ1  = document.getElementById('myImgJ1');
+var imgJ2  = document.getElementById('myImgJ2');
+var imgJ3  = document.getElementById('myImgJ3');
+var imgJ4  = document.getElementById('myImgJ4');
+var imgJ5  = document.getElementById('myImgJ5');
 var img  = document.getElementById('myImg');
 var img2 = document.getElementById('myImg2');
 var img3 = document.getElementById('myImg3');
@@ -15,9 +19,9 @@ imgJ1.onclick = function(){
     modalImg.src = img.src;
     captionText.innerHTML = this.alt;
 }
-img2.onclick = function(){
+imgJ2.onclick = function(){
     modal.style.display = "block";
-    modalImg.src = this.src;
+    modalImg.src = img2.src;
     captionText.innerHTML = this.alt;
 }
 img3.onclick = function(){
